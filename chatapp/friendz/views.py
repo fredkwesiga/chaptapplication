434cb11dc4836bfdf0801ba574f7ecb6da03ed2a
fredkwesiga <fredkwesiga9@gmail.com>
2024-05-15 10:42:17 +0300

    adds models to the app

--- chatapp/friendz/views.py
+++ chatapp/friendz/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from chat.models import Room, Message
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
